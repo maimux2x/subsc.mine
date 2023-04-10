9a69f5f5ed4ebc053a58649bc7b53a04f57227f7
maimux2x <m0smile10often@gmail.com>
2023-04-10 13:48:36 +0900

    googleアカウントでログインし、ログアウトできるようにした

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
   validates :uid, uniqueness: { scope: :provider }
 
-  def self.find_from_omniauth(auth_hash)
+  def self.from_omniauth(auth_hash)
     provider = auth_hash[:provider]
     uid = auth_hash[:uid]
-    User.where(provider:, uid:).first
+    User.where(provider:, uid:).first_or_initialize
   end
 end
