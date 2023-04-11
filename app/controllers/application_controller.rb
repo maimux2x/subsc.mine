82adacc35d4b3c3e192c3f73002b6c5e834ade25
maimux2x <m0smile10often@gmail.com>
2023-04-11 16:01:41 +0900

    ログインしていないのにヘッダーにログアウトボタンが表示されているバグを修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  helper_method :current_user
+  helper_method :logged_in?
 
-  def current_user
-    User.find(session[:user_id]) if session[:user_id]
+  private
+
+  def logged_in?
+    !!session[:user_id]
   end
 end
