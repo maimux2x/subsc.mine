5361baae22ff96b71cc455e2c836b339554d435b
maimux2x <m0smile10often@gmail.com>
2023-04-15 17:48:13 +0900

    Subscriptionのテストデータが間違っていたのを修正

--- spec/factories/subscriptions.rb
+++ spec/factories/subscriptions.rb
@@ -1,11 +1,11 @@
 FactoryBot.define do
   factory :subscription do
     user
-    sequence(:name) {|i| "サブスクリプション名"}    
-    sequence(:payment_date) {|i| rand(1..30).days.from_now}    
-    sequence(:fee) {|i| 500}    
-    sequence(:my_account_url) {|i| "https://example.com/user"}    
-    sequence(:subscribed) {|i| true}    
-    sequence(:cycle) {|i| 1}    
+    sequence(:name) { |i| "サブスクリプション名#{i}" }
+    sequence(:payment_date) { rand(1..30).days.from_now }
+    sequence(:fee) { 500 }
+    sequence(:my_account_url) { |i| "https://example.com/#{i}" }
+    sequence(:subscribed) { true }
+    sequence(:cycle) { 1 }
   end
 end
