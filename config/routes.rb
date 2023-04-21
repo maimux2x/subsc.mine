a0391da26a6354a1f779127418310bfc650f8a5e
maimu <76797372+maimux2x@users.noreply.github.com>
2023-04-21 09:11:32 +0900

    Revert "Vue.js導入用にAPIを用意した"

--- config/routes.rb
+++ config/routes.rb
@@ -10,12 +10,6 @@ Rails.application.routes.draw do
   resources :sessions, only: %i(new create destroy)
   resources :welcome, only: %i(index)
   resources :subscriptions, only: %i(index new create edit update destroy)
-
-  namespace :api, {format: 'json'} do
-    namespace :v1 do
-      resources :subscriptions, only: %i(index destroy)
-    end
-  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
