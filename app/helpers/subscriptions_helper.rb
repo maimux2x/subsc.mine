d75b5267db408a80fd4f208d9b009bd59e3d8c80
maimux2x <m0smile10often@gmail.com>
2023-04-24 10:37:10 +0900

    単発のデータをGoogleカレンダーに飛ばす処理を実装した

--- app/helpers/subscriptions_helper.rb
+++ app/helpers/subscriptions_helper.rb
@@ -1,2 +1,13 @@
 module SubscriptionsHelper
+  def google_calendar_url(subscription)
+    uri = URI('http://www.google.com/calendar/render')
+    uri.query =
+      {
+        action: 'TEMPLATE',
+        text: subscription.name,
+        dates: "#{subscription.calc_next_payment_date.strftime('%Y%m%dT%H%M%S')}/#{subscription.calc_next_payment_date.strftime('%Y%m%dT%H%M%S')}",
+        details: "http://localhost/#{subscription.id}"
+      }.to_param
+    uri.to_s
+  end
 end
