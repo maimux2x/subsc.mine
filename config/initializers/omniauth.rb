d060264675dbd57def91596f788b6f8b082bfbf9
maimux2x <m0smile10often@gmail.com>
2023-04-25 19:58:31 +0900

    デプロイエラーのため環境変数を修正

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,9 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   if Rails.env.development? || Rails.env.test?
     provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET']
-  else
-    provider :google_oauth2,
-    Rails.application.credentials.google[:client_id],
-    Rails.application.credentials.google[:client_secret]
-  end
 end
