2458755e2c211f5614c5f636e29d6f394b4d1469
maimux2x <m0smile10often@gmail.com>
2023-04-26 14:48:18 +0900

    サブスクリプション用のテストデータの見直しをした

--- spec/factories/subscriptions.rb
+++ spec/factories/subscriptions.rb
@@ -1,11 +1,13 @@
 FactoryBot.define do
   factory :subscription do
     user
+    start_month = Date.parse("2023/04/01")
+    end_month = Date.parse("2023/04/30")
     sequence(:name) { |i| "サブスクリプション名#{i}" }
-    sequence(:payment_date) { rand(1..30).days.from_now }
-    sequence(:fee) { 500 }
+    sequence(:payment_date) { Random.rand(start_month..end_mont) }
+    sequence(:fee) { rand(0..1000000) }
     sequence(:my_account_url) { |i| "https://example.com/#{i}" }
     sequence(:subscribed) { true }
-    sequence(:cycle) { 1 }
+    sequence(:cycle) { [1, 2, 3, 6, 12].sample }
   end
 end
