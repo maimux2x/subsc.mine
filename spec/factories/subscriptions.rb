d44016101e9aa1ba091e9ff594f7dcc2f0fd2504
maimux2x <m0smile10often@gmail.com>
2023-04-26 14:53:21 +0900

    変数名と数値の区切りを修正

--- spec/factories/subscriptions.rb
+++ spec/factories/subscriptions.rb
@@ -1,11 +1,11 @@
 FactoryBot.define do
   factory :subscription do
     user
-    start_month = Date.parse("2023/04/01")
-    end_month = Date.parse("2023/04/30")
+    from = Date.parse("2023/04/01")
+    to = Date.parse("2023/04/30")
     sequence(:name) { |i| "サブスクリプション名#{i}" }
-    sequence(:payment_date) { Random.rand(start_month..end_mont) }
-    sequence(:fee) { rand(0..1000000) }
+    sequence(:payment_date) { Random.rand(from..to) }
+    sequence(:fee) { rand(0..1_000_000) }
     sequence(:my_account_url) { |i| "https://example.com/#{i}" }
     sequence(:subscribed) { true }
     sequence(:cycle) { [1, 2, 3, 6, 12].sample }
