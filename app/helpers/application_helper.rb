844c9e9348689453ce62d59b547748dce665415a
maimux2x <m0smile10often@gmail.com>
2023-04-28 22:14:37 +0900

    OGPの追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,24 @@
+# frozen_string_literal: true
+
 module ApplicationHelper
+  def default_meta_tags
+    {
+      site: 'Subsc.mine',
+      reverse: true,
+      charset: 'utf-8',
+      description: 'Subsc.mineは複数のサブスクリプションサービスを契約している人向けの、利用サブスク一覧ツールです。',
+      og: {
+        title: :title,
+        type: 'website',
+        site_name: 'Subsc.mine',
+        description: :description,
+        image: image_url('OGP.png'),
+        url: 'https://subsc-mine.fly.dev/'
+      },
+      twitter: {
+        card: 'summary_large_image',
+        site: '@maimux2x'
+      }
+    }
+  end
 end
