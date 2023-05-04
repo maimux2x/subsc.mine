81c7ff3fccd178f8f407711b3d01b59107dba2ef
maimux2x <m0smile10often@gmail.com>
2023-05-04 13:36:23 +0900

    headタグのタイトルやメタディスクリプションが正しく反映されていなかったため修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@
 module ApplicationHelper
   def default_meta_tags
     {
-      site: 'Subsc.mine',
+      site: 'サブスクの次回お支払日を管理 Subsc.mine',
       reverse: true,
       charset: 'utf-8',
       description: 'Subsc.mineは複数のサブスクリプションサービスを契約している人向けの、利用サブスク一覧ツールです。',
@@ -12,8 +12,8 @@ module ApplicationHelper
         type: 'website',
         site_name: 'Subsc.mine',
         description: :description,
-        image: image_url('OGP.png'),
-        url: 'https://subsc-mine.fly.dev/'
+        image: image_url('OGP.jpg'),
+        url: 'https://subsc-mine.com/'
       },
       twitter: {
         card: 'summary_large_image',
