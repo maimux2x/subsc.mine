9a17586bd4b9d086fafdcb9fe35f977aa27745b8
maimux2x <m0smile10often@gmail.com>
2023-05-07 11:17:05 +0900

    カレンダー連携時のURLに含まれるuser_idをMD5で暗号化した

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  after_save :create_id_digest
   validates :uid, uniqueness: { scope: :provider }
 
   has_many :subscriptions, dependent: :destroy
@@ -9,5 +10,18 @@ class User < ApplicationRecord
     provider = auth_hash[:provider]
     uid = auth_hash[:uid]
     User.where(provider:, uid:).first_or_initialize
+  end
+
+  def to_param
+    id_digest
+  end
+
+  private
+
+  def create_id_digest
+    return unless id_digest.nil?
+
+    new_digest = Digest::MD5.hexdigest(id.to_s)
+    update_column(:id_digest, new_digest)
   end
 end
